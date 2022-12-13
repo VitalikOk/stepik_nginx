55a76499c11a1ac40efbf6975f96c7b2c292afa1
loop <loop@MacBook-Pro.local>
2022-12-13 22:47:31 +0300

    5

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-def app(env, start_response):
+def application(env, start_response):
     q_string = env.get('QUERY_STRING')
     params = q_string.split('&')
     start_response('200 OK', [('Content-Type', 'text/plain')])
