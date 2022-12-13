2f62c845d867013116ea3865a131245b2622c24a
loop <loop@MacBook-Pro.local>
2022-12-13 23:02:49 +0300

    5

--- hello.py
+++ hello.py
@@ -1,9 +1,4 @@
 def application(env, start_response):
-    q_string = env.get('QUERY_STRING')
-    params = q_string.split('&')
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    result = ''
-    for param in params:
-        result += param+'\n'
-    result = [param+"\r\n" for param in params]
-    return result
+    return [bytes(par+'\n', 'ascii') for par in env['QUERY_STRING'].split('&')]
+    
