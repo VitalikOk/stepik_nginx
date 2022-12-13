9e8055c8606906286342773f57a23b418e60541f
loop <loop@MacBook-Pro.local>
2022-12-13 23:20:59 +0300

    5

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/gunicorn.conf.py
 # sudo /etc/init.d/gunicorn restart
 # gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app
-gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:application &
+sudo touch /home/box/web/log/{gunicorn_access.log,gunicorn_error.log}
+sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:application &
 
 # sudo /etc/init.d/mysql start
