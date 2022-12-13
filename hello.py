24fd50e4319fb9fca63ba11bbd4b8d488fdfbd2d
loop <loop@MacBook-Pro.local>
2022-12-13 22:26:15 +0300

    5

--- hello.py
+++ hello.py
@@ -2,4 +2,8 @@ def app(env, start_response):
     q_string = env.get('QUERY_STRING')
     params = q_string.split('&')
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return params
+    result = ''
+    for param in params:
+        result += param+'\n'
+        
+    return [params.strp('\n')]
--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/gunicorn.conf.py
 # sudo /etc/init.d/gunicorn restart
 # gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app
 gunicorn -c /etc/gunicorn.d/test/gunicorn.conf.py hello
