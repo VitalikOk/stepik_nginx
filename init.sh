040f07c10f9d8abfed5d1d4e33c728f17707f875
loop <loop@MacBook-Pro.local>
2022-12-13 22:35:44 +0300

    5

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/gunicorn.conf.py   /etc/gunicorn.d/gunicorn.conf.py
 # sudo /etc/init.d/gunicorn restart
 # gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app
-gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello
+gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:app
 
 # sudo /etc/init.d/mysql start
