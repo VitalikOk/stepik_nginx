d68b6b7e93f8840aebbd2d3d5ee02e37df91dd6d
loop <loop@MacBook-Pro.local>
2022-12-16 11:51:06 +0300

    django was added

--- etc/gunicorn.conf.py
+++ etc/gunicorn.conf.py
@@ -1,5 +1,5 @@
 python='/usr/bin/python'                                           
-bind="0.0.0.0:8080"
+# bind="0.0.0.0:8080"
 accesslog='/home/box/web/log/gunicorn_access.log'
 errorlog='/home/box/web/log/gunicorn_error.log'
 pythonpath='/usr/bin/python'
