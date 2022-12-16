fb37276acbc768b0548b2545f2fcf45eff358d1c
loop <loop@MacBook-Pro.local>
2022-12-16 12:05:16 +0300

    init.sh was fixed addet rm of ex dirs

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo rm -rf /etc/gunicorn.d/gunicorn.conf.py
 sudo ln -s /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/gunicorn.conf.py
-sudo mkdir /home/box/web/{log,pid}/
+sudo mkdir -p /home/box/web/{log,pid}/
 sudo touch /home/box/web/log/{gunicorn_access.log,gunicorn_error.log}
 sudo touch /home/box/web/pid/gupid.pid
 # sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:application &
