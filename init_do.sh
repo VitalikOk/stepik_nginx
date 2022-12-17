8c58d9aab2f4bded1372c5e42b72b82e28972053
loop <loop@MacBook-Pro.local>
2022-12-17 23:58:49 +0300

    nk

--- init_do.sh
+++ init_do.sh
@@ -1,6 +1,6 @@
 sudo apt update                     # готовимся скачивать питон
-sudo apt install python3.5          # скачиваем питон
+sudo apt install python3.6          # скачиваем питон
 sudo rm /usr/bin/python3                         # удалить питон 3.4.3 из системы
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3   # поставить на его место питон 3.5
+sudo ln -s /usr/bin/python3.6 /usr/bin/python3   # поставить на его место питон 3.5
 sudo pip3 install gunicorn      # установить жуникорн
 sudo pip3 install django==2.1     
