2a021b93112f4bf2d162d9eda9b43e82abdaec09
loop <loop@MacBook-Pro.local>
2022-12-18 16:44:01 +0300

    p-35

--- 0_init.sh
+++ 0_init.sh
@@ -10,7 +10,7 @@ sudo python3 -m pip install gunicorn #sudo pip3 install gunicorn      # уста
 sudo python3 -m install django==2.1 #sudo pip3 install django==2.1  
 # sudo python3 -m pip install mysqlclient
 
-sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-pil python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev
+# sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-pil python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev libssl-dev
 
 
    
