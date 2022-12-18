a43c8ccea1990ffc1bc277f5566dbf8ec89f364b
loop <loop@MacBook-Pro.local>
2022-12-18 16:16:22 +0300

    fix 0

--- 0_init.sh
+++ 0_init.sh
@@ -1,7 +1,19 @@
-sudo apt update                     # готовимся скачивать питон
-sudo apt install python3.5       # скачиваем питон
-sudo rm /usr/bin/python3                         # удалить питон 3.4.3 из системы
+sudo apt update
+sudo apt install python3.5
+sudo apt install python3.5-dev    # скачиваем питон
+sudo rm /usr/bin/python3     #sudo unlink /usr/bin/python3  
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3   # поставить на его место питон 3.5
-sudo pip3 install gunicorn      # установить жуникорн
-sudo pip3 install django==2.1     
-# sudo python3 -m pip install mysqlclient
+
+sudo python3 -m pip install gunicorn #sudo pip3 install gunicorn      # установить жуникорн
+sudo python3 -m install django==2.1 #sudo pip3 install django==2.1  
+sudo python3 -m pip install mysqlclient
+
+
+
+
+   
+# sudo pip3 install pymysql
+# # sudo python3 -m pip install mysqlclient
+# sudo pip3 install mysql-python
+
+
