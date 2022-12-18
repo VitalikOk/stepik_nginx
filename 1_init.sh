561736fd23424162b6c0022b44a4d40de0ef547e
loop <loop@MacBook-Pro.local>
2022-12-18 13:23:25 +0300

    sep init

--- 1_init.sh
+++ 1_init.sh
@@ -7,10 +7,4 @@ sudo mkdir -p /home/box/web/{log,pid}/
 sudo touch /home/box/web/log/{gunicorn_access.log,gunicorn_error.log}
 sudo touch /home/box/web/pid/gupid.pid
 sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf.py hello:application --bind 0.0.0.0:8080 &
-sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ask/ask.wsgi:application --bind 0.0.0.0:8000 &
-sudo /etc/init.d/mysql start  
-mysql -uroot -e "create database stepik;"
-mysql -uroot -e "create user 'stepik'@'localhost' identified by 'stepik;"
-mysql -uroot -e "grant all privileges on  stepik.* to 'stepik'@'localhost'with grant option;"
-~/web/ask/manage.py makemigrations
-~/web/ask/manage.py migrate
+sudo gunicorn -c /etc/gunicorn.d/gunicorn.conf.py ~/web/ask/ask.wsgi:application --bind 0.0.0.0:8000 &
