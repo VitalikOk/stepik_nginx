760b1b23003d5385d2ce2db3e4d60de0c8238b70
loop <loop@MacBook-Pro.local>
2022-12-20 15:44:36 +0300

    gfed

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -19,13 +19,13 @@ import qa.views as qa
 
 
 urlpatterns = [
-   url(r'^$', 'new', name='new'),                                                              
-   url(r'^login/.*$', 'login', name='login'),                                    
-   url(r'^signup/.*', 'signup', name='signup'),                                   
-   url(r'^question/(?P<id>[0-9]+)/$', 'question', name='question'),                 
-   url(r'^ask/.*', 'ask', name='ask'),                                         
-   url(r'^popular/.*', 'popular', name='popular'),                                 
-   url(r'^new/.*', 'new', name='new'),                                          
+   url(r'^$', qa.new, name='new'),                                                              
+   url(r'^login/.*$', qa.login, name='login'),                                    
+   url(r'^signup/.*', qa.signup, name='signup'),                                   
+   url(r'^question/(?P<id>[0-9]+)/$', qa.question, name='question'),                 
+   url(r'^ask/.*', qa.ask, name='ask'),                                         
+   url(r'^popular/.*', qa.popular, name='popular'),                                 
+   url(r'^new/.*', qa.new, name='new'),                                          
 ]
 
 
