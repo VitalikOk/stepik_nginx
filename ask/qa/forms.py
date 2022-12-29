7b9d79073786f9729a58e02c4236729a0566c53f
loop <loop@MacBook-Pro.local>
2022-12-30 00:10:17 +0300

    reset

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from qa.models import Question, Answer
-from django.contrib.auth.models import User
-from django.db import IntegrityError
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.password_validation import validate_password
-from django.conf import settings
+
 
 class AskForm(forms.Form):
     """
@@ -13,7 +9,7 @@ class AskForm(forms.Form):
     """
     title = forms.CharField(max_length=100)
     text = forms.CharField(widget=forms.Textarea)
-    author = forms.CharField(max_length=50, blank = True)
+    
     
     def clean(self):
         if False:
@@ -26,14 +22,6 @@ class AskForm(forms.Form):
         question = Question(**self.cleaned_data)
         question.save()
         return question
-    
-    def clean_author(self):
-        author = self.cleaned_data['author']
-        try:
-            author = User.objects.get(username=author)
-        except:
-            return None
-        return author    
             
 
 
@@ -43,68 +31,15 @@ class AnswerForm(forms.Form):
     """
     text = forms.CharField(widget=forms.Textarea)
     question = forms.IntegerField(widget=forms.HiddenInput())
-    author = forms.CharField(max_length=50, blank = True)
     def clean(self):
         if False:
             raise forms.ValidationError(
                 u'Какая-то ошибка',
                 code='err'
     )
-            
-    def clean_author(self):
-        author = self.cleaned_data['author']
-        try:
-            author = User.objects.get(username=author)
-        except:
-            return None
-        return author    
             
     def save(self):
         question = Question.objects.get(id=self.cleaned_data['question'])
         answer = Answer(text=self.cleaned_data['text'],question=question)
         answer.save()
         return answer
-    
-    
-class SignupForm(forms.Form):
-    """
-    форма для регистрации
-    """
-    username = forms.CharField(max_length=20)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
-    
-    def clean(self):
-        if False:
-            raise forms.ValidationError(
-                u'Какая-то ошибка',
-                code='err'
-    )
-            
-    def clean_password(self):
-        password = self.cleaned_data['password']
-        # validate_password(password)
-        password = make_password(password, salt=settings.SECRET_KEY)
-        return password        
-            
-            
-    def save(self):
-        user = User(**self.cleaned_data)
-        try: 
-            user.save()
-            return user
-        except IntegrityError as e:
-            raise Exception(e)
-class SigninForm(forms.Form):
-    """
-    форма для логина
-    """
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(widget=forms.PasswordInput())
-    
-    def clean(self):
-        if False:
-            raise forms.ValidationError(
-                u'Какая-то ошибка',
-                code='err'
-    )
